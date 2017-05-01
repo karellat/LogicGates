1ac316945ed805af00e9a18da544d144014ec59c
Tomas Karella <tomas.karella@gmail.com>
2017-05-01 17:15:22 +0200

    User documentation remade, check size of parameters for Passive mode

--- LogicGates/LogicGates/main.cpp
+++ LogicGates/LogicGates/main.cpp
@@ -56,21 +56,21 @@ int main(int argc, char *argv[])
 	bool tryAllInputs = false;
 	int index = 1;
 	//Parse parameters:
-	while(index < argc)
+	while (index < argc)
 	{
 		if (std::strcmp(argv[index], "-d") == 0)
 		{
-			debugMode = true; 
+			debugMode = true;
 			index++;
 		}
-		else if(std::strcmp(argv[index],"-f") == 0)
+		else if (std::strcmp(argv[index], "-f") == 0)
 		{
 			index++;
-			while(index < argc && !argumentTest(argv[index]))
+			while (index < argc && !argumentTest(argv[index]))
 			{
 				fileNames.push_back(argv[index]);
 				index++;
-					
+
 			}
 		}
 		else if (std::strcmp(argv[index], "-i") == 0) {
@@ -78,9 +78,9 @@ int main(int argc, char *argv[])
 			while (index < argc && !argumentTest(argv[index]))
 			{
 				try {
-					if(!tryAllInputs) inputs.push_back(stringToBools(argv[index]));
+					if (!tryAllInputs) inputs.push_back(stringToBools(argv[index]));
 				}
-				catch(invalidinputformat& e)
+				catch (invalidinputformat& e)
 				{
 					std::cout << e.what() << endl;
 					return 1;
@@ -94,21 +94,28 @@ int main(int argc, char *argv[])
 			index++;
 			std::cout << "Logic Gates" << endl;
 			std::cout << "DESCRIPTIONS:" << endl;
-			std::cout << "\tArguments: " << endl; 
+			std::cout << "\tArguments: " << endl;
 			std::cout << "\t-h \tprint help " << endl;
-			std::cout << "\t-f [file..] \tconstruction files given to construct in given order" << endl; 
+			std::cout << "\t-f [file..] \tconstruction files given to construct in given order" << endl;
 			std::cout << "\t-i [STRING..] \tinputs for last constructed logic network, 1 - for true, 0 - for false, if wrong format throws exception " << endl;
 			std::cout << "\t-a \t generate all possible outputs and ignore previous inputs read afer -i" << endl;
 		}
-		else if (std::strcmp(argv[index],"-a") == 0)
+		else if (std::strcmp(argv[index], "-a") == 0)
 		{
 			index++;
 			tryAllInputs = true;
 		}
 	}
 	//Run workbench in passive mode: 
-	w = make_unique<WorkbenchTUI>(cout.rdbuf(), cin.rdbuf(), cout.rdbuf(),debugMode);
-	w->PassiveMode(std::move(fileNames), std::move(inputs),tryAllInputs);
+	if (!fileNames.empty() && (!inputs.empty() || tryAllInputs))
+	{		w = make_unique<WorkbenchTUI>(cout.rdbuf(), cin.rdbuf(), cout.rdbuf(), debugMode);
+	w->PassiveMode(std::move(fileNames), std::move(inputs), tryAllInputs);
+	}
+	else
+	{
+		std::cout << "At least one construction file & at least one input has to be given " << endl;
+	}
+	std::cout << "Press ENTER to exit" << endl;
 	getline(cin, blank);
 }
 
